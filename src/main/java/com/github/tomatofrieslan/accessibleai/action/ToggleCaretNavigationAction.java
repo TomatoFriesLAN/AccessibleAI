a149cc64a70a0ea3cda138357644e1f2d6620ec1
TomatoFriesLAN <94922062+TomatoFriesLAN@users.noreply.github.com>
2025-03-23 14:25:43 -0400

    add dummy keyboard shortcut

---
+++ src/main/java/com/github/tomatofrieslan/accessibleai/action/ToggleCaretNavigationAction.java
@@ -0,0 +1,29 @@
+package com.github.tomatofrieslan.accessibleai.action;
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.pom.Navigatable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class ToggleCaretNavigationAction extends AnAction {
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return ActionUpdateThread.BGT;
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+        Project currentProject = anActionEvent.getProject();
+        Messages.showMessageDialog(
+                currentProject,
+                "we did it",
+                "we did it",
+                Messages.getInformationIcon());
+    }
+}
