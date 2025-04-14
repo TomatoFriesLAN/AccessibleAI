6a29d062db8b3b5240a905551e135aeef8a75792
TomatoFriesLAN <94922062+TomatoFriesLAN@users.noreply.github.com>
2025-04-13 23:18:23 -0400

    follow link hovered by caret

--- src/main/java/com/github/tomatofrieslan/accessibleai/action/CreateTestNotificationAction.java
+++ src/main/java/com/github/tomatofrieslan/accessibleai/action/CreateTestNotificationAction.java
@@ -7,9 +7,14 @@ import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.keymap.impl.ui.KeymapPanel;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.ui.popup.ListPopup;
 import com.intellij.platform.lang.lsWidget.OpenSettingsAction;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
 
 public class CreateTestNotificationAction extends AnAction {
     public @NotNull ActionUpdateThread getActionUpdateThread() {
@@ -18,9 +23,17 @@ public class CreateTestNotificationAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        Notification notification = new Notification("Accessible AI Notifications", "Test notification", "This notification will be more useful in the future.", NotificationType.INFORMATION);
+        FocusManager focusManager = FocusManager.getCurrentManager();
+
+        Notification notification = new Notification("Accessible AI Notifications", "Test notification", focusManager.getFocusOwner().toString(), NotificationType.INFORMATION);
         notification.addAction(new OpenSettingsAction(KeymapPanel.class, "Keymap...", AllIcons.General.Settings));
 
         Notifications.Bus.notify(notification);
+
+        ListPopup popup = JBPopupFactory.getInstance().createActionGroupPopup(
+                "getPopupTitle(e)", new DefaultActionGroup(), e.getDataContext(), JBPopupFactory.ActionSelectionAid.ALPHA_NUMBERING, true, null, -1,
+                null, "what is this");
+
+        popup.showCenteredInCurrentWindow(e.getProject());
     }
 }
--- src/main/java/com/github/tomatofrieslan/accessibleai/action/FollowLinkAction.java
+++ src/main/java/com/github/tomatofrieslan/accessibleai/action/FollowLinkAction.java
@@ -1,18 +1,26 @@
 package com.github.tomatofrieslan.accessibleai.action;
 
 import com.github.tomatofrieslan.accessibleai.services.KeyboardNavigationService;
+import com.intellij.icons.AllIcons;
 import com.intellij.ml.llm.core.chat.ui.chat.TextPartViewEditorPane;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.keymap.impl.ui.KeymapPanel;
 import com.intellij.openapi.project.Project;
+import com.intellij.platform.lang.lsWidget.OpenSettingsAction;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.text.AttributeSet;
+import javax.swing.text.Element;
 import javax.swing.text.html.HTML;
 import javax.swing.text.html.HTMLDocument;
+import java.awt.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -31,13 +39,34 @@ public class FollowLinkAction extends AnAction {
                 "we did it",
                 Messages.getInformationIcon());
 
-         */
+
         JEditorPane messagePart = ((TextPartViewEditorPane) KeyboardNavigationService.getAIAssistantChatMessageView(currentProject, 1).getMessage().getAllParts().get(0).getComponent());
         HTMLDocument document = ((HTMLDocument) messagePart.getDocument());
         HTMLDocument.Iterator iterator = document.getIterator(HTML.Tag.A);
         int startOffset = iterator.getStartOffset();
         //messagePart.hasFocus();
         //Element element = document.getCharacterElement(messagePart.getCaretPosition());
+
+         */
+
+        FocusManager focusManager = FocusManager.getCurrentManager();
+        Component component = focusManager.getFocusOwner();
+        if (component instanceof TextPartViewEditorPane editorPane) {
+            int caretPosition = editorPane.getCaretPosition();
+            HTMLDocument htmlDocument = (HTMLDocument) editorPane.getDocument();
+            Element element = htmlDocument.getCharacterElement(caretPosition);
+            Object attribute = element.getAttributes().getAttribute(HTML.Tag.A);
+            if (attribute instanceof AttributeSet attributeSet) {
+                URL url;
+                try {
+                    url = new URL((String) attributeSet.getAttribute(HTML.Attribute.HREF));
+                } catch (MalformedURLException e) {
+                    url = null;
+                }
+                editorPane.fireHyperlinkUpdate(new HyperlinkEvent(editorPane, HyperlinkEvent.EventType.ACTIVATED, url, "is this working", element));
+            }
+        }
+        /*
         URL url;
         try {
             url = new URL((String) iterator.getAttributes().getAttribute(HTML.Attribute.HREF));
@@ -45,5 +74,6 @@ public class FollowLinkAction extends AnAction {
             url = null;
         }
         messagePart.fireHyperlinkUpdate(new HyperlinkEvent(messagePart, HyperlinkEvent.EventType.ACTIVATED, url, "is this working", document.getCharacterElement(startOffset)));
+        */
     }
 }
